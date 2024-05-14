8140a7f8a735f4030126123c379564a35094157b
Anurag Joshi <148903459+anuragJoshi05@users.noreply.github.com>
2024-05-14 19:02:47 +0530

    Updated bar.dart

--- lib/bar.dart
+++ lib/bar.dart
@@ -1,115 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:opso/about.dart';
-import 'package:url_launcher/link.dart';
 
 class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
   const AppBarWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'OpSo',
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        ),
-      ),
-      body: Material(
-        color: Colors.transparent,
-        child: Ink(
-          decoration: BoxDecoration(
-            color: Colors.white,
-          ),
-          child: InkWell(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: ListView(
-                children: [
-                  MenuOption(
-                    title: 'About',
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => AboutScreen()),
-                      );
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
+    return AppBar(
+      title: const Text(
+        'OpSo',
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,),
       ),
     );
   }
 
   @override
-  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
 
-class MenuOption extends StatefulWidget {
+class MenuOption extends StatelessWidget {
   final String title;
   final VoidCallback onTap;
 
   const MenuOption({
+    Key? key,
     required this.title,
     required this.onTap,
-  });
-
-  @override
-  _MenuOptionState createState() => _MenuOptionState();
-}
-
-class _MenuOptionState extends State<MenuOption> {
-  bool _isClicked = false;
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          _isClicked = !_isClicked;
-        });
-        widget.onTap();
-      },
-      child: Material(
-        child: Ink(
-          decoration: BoxDecoration(
-            color: Color.fromARGB(255, 237, 237, 239),
-            borderRadius: BorderRadius.circular(25),
-          ),
-          child: InkWell(
-            borderRadius: BorderRadius.circular(20),
-            onTap: () {
-              setState(() {
-                _isClicked = !_isClicked;
-              });
-              widget.onTap();
-            },
-            child: Container(
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Text(
-                        widget.title,
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 220,
-                    ),
-                    Icon(Icons.arrow_forward_ios),
-                  ],
-                ),
+    return InkWell(
+      borderRadius: BorderRadius.circular(25),
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
               ),
             ),
-          ),
+            const Icon(Icons.arrow_forward_ios),
+          ],
         ),
       ),
     );
